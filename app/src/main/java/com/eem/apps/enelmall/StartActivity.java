9a6c1647fe05f175af3a88223e2daf32e8431808
ranfis <ranfis.sh@gmail.com>
2015-02-26 02:28:04 -0400

    Create offers activity

--- app/src/main/java/com/eem/apps/enelmall/StartActivity.java
+++ app/src/main/java/com/eem/apps/enelmall/StartActivity.java
@@ -1,7 +1,11 @@
 package com.eem.apps.enelmall;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.graphics.Typeface;
+import android.widget.TextView;
 
 
 public class StartActivity extends Activity {
@@ -10,5 +14,16 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        // Change Typeface for app name
+        String fontPath = "fonts/Kraftstoff_Regular.otf";
+        TextView txtAppName = (TextView)findViewById(R.id.txtAppName);
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+        txtAppName.setTypeface(tf);
+    }
+
+    public void gotoOffers(View view) {
+        Intent offers = new Intent(this,OffersActivity.class);
+        startActivity(offers);
     }
 }
