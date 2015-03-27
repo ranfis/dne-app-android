87e5bab329edd4191a8f574302bc1a7382d24998
ranfis <ranfis.sh@gmail.com>
2015-03-27 11:08:44 -0400

    Lookup location in onCreate()

--- app/src/main/java/com/eem/apps/enelmall/StartActivity.java
+++ app/src/main/java/com/eem/apps/enelmall/StartActivity.java
@@ -1,5 +1,4 @@
 package com.eem.apps.enelmall;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,34 +7,23 @@ import android.graphics.Typeface;
 import android.widget.TextView;
 import android.location.Location;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 
-public class StartActivity extends Activity implements
-        ConnectionCallbacks, OnConnectionFailedListener {
 
-    protected static final String TAG = "basic-location-sample";
-
-    /**
-     * Provides the entry point to Google Play services.
-     */
+public class StartActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
+    protected static final String TAG = "[StartActivity]";
     protected GoogleApiClient mGoogleApiClient;
-
-    /**
-     * Represents a geographical location.
-     */
     protected Location mLastLocation;
-
     static StartActivity self;
     public final static String OFFERS = "com.eem.apps.enelmall.OFFERS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("StartActivity","onCreate()");
+        Log.d(TAG,"onCreate()");
         super.onCreate(savedInstanceState);
         self = this;
         setContentView(R.layout.activity_start);
@@ -51,12 +39,13 @@ public class StartActivity extends Activity implements
         //new OffersBatch().execute(urlString);
 
         buildGoogleApiClient();
+        mGoogleApiClient.connect();
     }
 
-    /**
-     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
-     */
+
+    // Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
     protected synchronized void buildGoogleApiClient() {
+        Log.d(TAG,"buildGoogleApiClient()");
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -64,11 +53,6 @@ public class StartActivity extends Activity implements
                 .build();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
 
     @Override
     protected void onStop() {
@@ -78,44 +62,40 @@ public class StartActivity extends Activity implements
         }
     }
 
-    /**
-     * Runs when a GoogleApiClient object successfully connects.
-     */
+
+    // Runs when a GoogleApiClient object successfully connects.
     @Override
     public void onConnected(Bundle connectionHint) {
-        // Provides a simple way of getting a device's location and is well suited for
-        // applications that do not require a fine-grained location and that do not need location
-        // updates. Gets the best and most recent location currently available, which may be null
-        // in rare cases when a location is not available.
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation != null) {
            String lat = String.valueOf(mLastLocation.getLatitude());
             String lon = String.valueOf(mLastLocation.getLatitude());
-            Toast.makeText(this, "lat: "+lat, Toast.LENGTH_LONG).show();
-            Toast.makeText(this, "lon: "+lon, Toast.LENGTH_LONG).show();
 
         } else {
             Toast.makeText(this, "No Location, Sorry :(", Toast.LENGTH_LONG).show(); // TODO: Prompt user to enable location
         }
     }
 
+
     @Override
     public void onConnectionFailed(ConnectionResult result) {
-        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-        // onConnectionFailed.
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+        Log.d(TAG,"onConnectionFailed()");
+        Log.e(TAG, "onConnectionFailed(): ConnectionResult.getErrorCode() = " + result.getErrorCode());
     }
 
 
     @Override
     public void onConnectionSuspended(int cause) {
+        Log.d(TAG,"onConnectionSuspended()");
         // The connection to Google Play services was lost for some reason. We call connect() to
         // attempt to re-establish the connection.
         Log.i(TAG, "Connection suspended");
         mGoogleApiClient.connect();
     }
 
+
     public static void goToOffers(String offersJson){
+        Log.d(TAG,"goToOffers()");
         Intent offers = new Intent(self,OffersActivity.class);
         offers.putExtra(OFFERS, offersJson);
         self.startActivity(offers);
