65484d24f7b727807cd6b98abcef41b9713f284d
ranfis <ranfis.sh@gmail.com>
2015-04-12 14:22:51 -0400

    All Malls class and refactor Store model

---
+++ app/src/main/java/com/eem/apps/enelmall/model/Mall.java
@@ -0,0 +1,37 @@
+package com.eem.apps.enelmall.model;
+
+public class Mall {
+    private int id;
+    private String name;
+    private Object location;
+
+    public Mall(int id,String name, Object location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getLocation() {
+        return location;
+    }
+
+    public void setLocation(Object location) {
+        this.location = location;
+    }
+}
--- app/src/main/java/com/eem/apps/enelmall/model/Store.java
+++ app/src/main/java/com/eem/apps/enelmall/model/Store.java
@@ -4,10 +4,14 @@ public class Store {
     private int id;
     private String name;
     private double rating;
+    private String hoursOpen;
+    private String dayssOpen;
 
-    public Store(int id, String name) {
+    public Store(int id, String name,String hoursOpen,String dayssOpen) {
         this.id = id;
         this.name = name;
+        this.setHoursOpen(hoursOpen);
+        this.setDayssOpen(dayssOpen);
     }
 
     public String getName() {
@@ -24,5 +28,21 @@ public class Store {
 
     public void setRating(double rating) {
         this.rating = rating;
+    }
+
+    public String getHoursOpen() {
+        return hoursOpen;
+    }
+
+    public void setHoursOpen(String hoursOpen) {
+        this.hoursOpen = hoursOpen;
+    }
+
+    public String getDayssOpen() {
+        return dayssOpen;
+    }
+
+    public void setDayssOpen(String dayssOpen) {
+        this.dayssOpen = dayssOpen;
     }
 }
